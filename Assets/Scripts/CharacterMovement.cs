4a9678c31b5979ff750c3f52f08eedb0e7751cbf
Kacper Duda <kacper.d.duda@gmail.com>
2019-12-09 22:55:09 +0100

    Update CharacterMovement.cs

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,7 +11,6 @@ using UnityEngine.PostProcessing;
         public event Action OnSensingFinishedEvent;
         public float range = 100f;    
 
-
         [SerializeField]
         Animator animator;
 
