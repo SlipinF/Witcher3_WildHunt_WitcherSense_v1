f8d7582b31d6a2ac3c0912f0a57f9957151cf2a9
Kacper Duda <kacper.d.duda@gmail.com>
2020-01-05 19:37:10 +0100

    Update CharacterMovement.cs

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,8 +6,6 @@ using UnityEngine.PostProcessing;
 
   public class CharacterMovement : MonoBehaviour
   {         
-  
-  
     public event Action OnSensingEvent;  
         public event Action OnSensingFinishedEvent;
         public float range = 100f;    
